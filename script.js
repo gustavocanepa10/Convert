f4ca46b7f177c89247b3f2518133c9c0da029f57
Gustavo Canepa <gustavociscotto@gmail.com>
2024-10-16 10:01:12 -0300

    Adicionando o formatador de real

--- script.js
+++ script.js
@@ -11,22 +11,30 @@ const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector("main footer")
 const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 
 // Manipulando o input "amount" para receber somente números
-amount.addEventListener("input",() => {
-    // console.log(amount.value)
+amount.addEventListener("input", (event) => {
     const regexCharacter = /\D+/g // expressão regular para receber somente números
     amount.value = amount.value.replace(regexCharacter, "")
+    console.log(amount.value)
 })
+    
+
 
 // Captando o evento de submit (enviar) do formulário
+
 form.addEventListener("submit", (event) => {
     event.preventDefault()
+    console.log(currency.value)
 
-    switch(currency.value) {
+
+
+
+    switch(currency.value) {  // amount, coinprice e symbol
         case "USD":
-            convertCurrency(amount.value, USD, "US$")
+            convertCurrency(amount.value, USD, "US$") 
             break
 
         case "EUR":
@@ -38,6 +46,11 @@ form.addEventListener("submit", (event) => {
             break
     }
 
+})
+
+
+    
+
   
     // console.log(amount.value)
     // console.log(currency.value)
@@ -45,18 +58,34 @@ form.addEventListener("submit", (event) => {
     
     
 
-})
+
 
 
 // Função para converter a moeda
-function convertCurrency(amount,price,symbol) {
+function convertCurrency(amount,coinprice,symbol) {
 
-    
+    // A lógica por trás desse try-catch é:
+    // se a conversão for bem sucedida o try adiciona a classe com o footer do resultado
+    // se não der certo o catch remove a classe e informa uma mensagem de erro
 
     
     try {
+        
 
-      description.textContent = `${symbol},1 = ${price}`
+        // Exibindo o conteúdo do valor exibido pelo footer
+       description.textContent = `${symbol} 1 = ${formatCurrencyBRL(coinprice)}`
+        
+        // calcula o total
+        total = amount * coinprice
+
+        // exibe o total
+        result.textContent = total
+
+        
+
+        
+
+      
 
 
 
@@ -74,5 +103,20 @@ function convertCurrency(amount,price,symbol) {
         
     }
     
+
+}
+
+
+// Função que formata o coinprice em moeda real brasileiro
+function formatCurrencyBRL(value) {
+    // Converte para Number o value
+    // ToLocalString nesse caso formata e retorna a moeda em real, 
+    // define o estilo que é currency
+    // e o tipo de currency, que é "BRL" 
+    return Number(value).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+
+    })
 
 }
